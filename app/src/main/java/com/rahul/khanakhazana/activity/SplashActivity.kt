464c94029a5daca0f93c1c698dcba053d1b80dfa
Rahul Kumar <rahulrai2822march@gmail.com>
2021-06-07 08:14:48 +0530

    splash layout added

---
+++ app/src/main/java/com/rahul/khanakhazana/activity/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.rahul.khanakhazana.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rahul.khanakhazana.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
